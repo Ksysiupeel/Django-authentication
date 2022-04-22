2f154f5f93c3f0555291e82f2de1ac953eaefa5b
Christopher <ksysiupeel@gmail.com>
2022-04-22 21:45:05 +0200

    Add register view, change button to submit, delete bugs

--- auth/views.py
+++ auth/views.py
@@ -1,14 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpRequest
 from .forms import RegisterForm
+from django.contrib.auth import login, logout, authenticate
 
-# Create your views here.
+
 def home(request: HttpRequest):
     return render(request, "index.html")
 
 def register(request: HttpRequest):
     if request.method == "POST":
         form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect("/home")
     else:
         form = RegisterForm()
     
